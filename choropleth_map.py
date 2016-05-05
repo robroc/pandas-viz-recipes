07e5025a741efe7bdb6cef3a6eefa2a5412de340
robroc <robroc1@gmail.com>
2016-05-05 13:44:18 -0400

    Better legend in choropleth

--- choropleth_map.py
+++ choropleth_map.py
@@ -6,11 +6,13 @@ from mpl_toolkits.basemap import Basemap
 from pysal.esda.mapclassify import Natural_Breaks as nb
 
 
-def make_choro(shapefile, dataframe, val_col, index_col, num_breaks = 6, color_ramp = 'Greens' title = '', main_poly = None, save = False):
+def make_choro(shapefile, dataframe, val_col, index_col, num_breaks = 6, color_ramp = 'Blues' title = '', main_poly = None, save = False):
     """
-    Makes a choropelth map of a given shapefile and a numerical column of a dataframe. Shapefile and
-    dataframe must both have a matching index_column for joining. Uses Jenks natural breaks for 
-    classifying.
+    Makes a choropelth map of a given shapefile and a numerical column (val_col) of a dataframe. 
+    Shapefile and dataframe must both have a matching index_column for joining. 
+    Uses Jenks natural breaks by PySAL for classifying.
+    
+    If you want to highlight a polygon differently, pass it to main_poly and uncomment lines 43-47.
     
     Based on this tutorial: http://ramiro.org/notebook/basemap-choropleth/
     """
@@ -20,11 +22,11 @@ def make_choro(shapefile, dataframe, val_col, index_col, num_breaks = 6, color_r
 	scheme = [cm(i / num_colors) for i in range(num_colors)]
     
     # Create bins for color values
-    breaks = nb( dataframe[val_col], initial=150, k = num_colors - 1)
+    breaks = nb( dataframe[val_col], initial=200, k = num_colors - 1)
     bins = breaks.bins
-    frame['bin'] = np.digitize(dataframe[val_col], bins) 
+    frame['bin'] = breaks.yb
 
-    mpl.style.use('ggplot')
+    mpl.style.use('seaborn-muted')
     fig = plt.figure(figsize=(22, 12))
 
     ax = fig.add_subplot(111, axisbg='w', frame_on=False)
@@ -57,8 +59,19 @@ def make_choro(shapefile, dataframe, val_col, index_col, num_breaks = 6, color_r
     # Draw color legend.
     ax_legend = fig.add_axes([0.35, 0.19, 0.3, 0.03], zorder=3)
     cmap = mpl.colors.ListedColormap(scheme)
-    cb = mpl.colorbar.ColorbarBase(ax_legend, cmap=cmap, ticks=bins, boundaries=bins, orientation='horizontal')
-    cb.ax.set_xticklabels([str(round(i, 1)) for i in bins])
+    
+    # Set labels for legends and round bins to nearest 100
+    legend_labels = np.linspace(dataframe[val_col].min(), dataframeframe[val_col].max(), num_colors)
+    legend_bins = np.around(bins, decimals = -2)
+    cb = mpl.colorbar.ColorbarBase(ax_legend, 
+                                   cmap=cmap, 
+                                   ticks=bins, 
+                                   boundaries=bins, 
+                                   orientation='horizontal' )
+    
+    cb.set_ticks(legend_labels[1:])
+    cb.ax.set_xticklabels(['{0:,}'.format(int(round(i, 1))) for i in bins])
+    cb.ax.tick_params(labelsize = 16) 
     if save:
         save_name = title.replace(' ', '_') + '.png'
         plt.savefig(save_name, dpi=100)
--- horizontal_bar.py
+++ horizontal_bar.py
@@ -1,19 +1,38 @@
 import pandas as pd
 import matplotlib.pypolt as plt
+import matplotlib.ticker as tkr
+
 
 plt.style.use('ggplot')
 
-def bar_h(dataframe, col, n = 12, title = '', xlabel='', save = False):
+def bar_h(dataframe, col, n = 12, title = None, xlabel='', save = False):   
+    ax = dataframe.sort(col).tail(n)[col].plot(kind="barh", 
+                                               figsize=[8, 5], 
+                                               color='#B5022C', 
+                                               width = 0.7, 
+                                               grid=False)
+                                               
     '''
     Function to draw a simple horizontal bar chart. Col can be one column of list of columns.
-    Bar labels will be the dataframe index.
+    Y-axis labels will be the dataframe index.
     '''
-    
-    ax = dataframe.sort(col).tail(n)[col].plot(kind='barh', figsize=[8, 5])
+                                               
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel('')
+    ax.set_axis_bgcolor('white')
+    ax.xaxis.grid(color = 'grey', linestyle='--')
+    ax.xaxis.tick_bottom()
+    
+    # Format x-axis ticks with thousands separator
+    x_format = tkr.FuncFormatter('{:,.0f}'.format)
+    ax.xaxis.set_major_formatter(x_format)
+    
+    ax.yaxis.tick_left()
+    ax.tick_params(axis="y", labelsize=12)
+    plt.tight_layout()
+    
     if save:
-        save_name = title.replace(' ', '_') + '.png'
-        plt.savefig(save_name, dpi=100)
+        file_name = title.replace(' ', '_') + '.png'
+        plt.savefig(file_name, dpi=100)
     plt.show()
